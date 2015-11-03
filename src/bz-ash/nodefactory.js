c28cdc64d0632fc8f3945c8ab2930d1e18dcce6a
sunrha <admin@sunrha>
2015-11-03 15:51:35 -0800

    nodefactory implemented; tests passing

--- src/bz-ash/nodefactory.js
+++ src/bz-ash/nodefactory.js
@@ -1,69 +1,45 @@
 var stampit = require('stampit'),
-  DEFAULT_TYPE = "AnonymousNodeStamper"
+  DEFAULT_TYPE_NAME = "AnonymousNodeFactory"
 
+var testtriggertest = ""
 
-module.exports = function(config) {
-    var typeName = config ? (config.typeName || DEFAULT_TYPE) : DEFAULT_TYPE,
-      prototypeType = { name: typeName }
-      res = stampit()
-          .refs({
-            type: prototypeType,             
-            componentTypes: []
-          })
-          .props({
-              previous: undefined,
-              next: undefined,
-              entity: {}                    
-          })
-          .init(function initTypes(param) {
-            if(config && config.componentTypes) {
-              //for each type put it on the type list for this guy
-              obj = param.instance
-              
-              config.componentTypes.forEach(function(t) {
-                this.componentTypes.push(t)
-              }, obj)        
-            }
-          })          
-          .static({
-            type: prototypeType
-          })
-          .methods({
-            hasComponentType: function hasComponentType(t) {
-              return this.componentTypes.indexOf(t) > -1
-            }
-          })
-
-  
-    // var className =  config ? (config.name || DEFAULT_NAME) : DEFAULT_NAME
-    // var res = stampit()
-    //             .props({
-    //                 previous: undefined,
-    //                 next: undefined,
-    //                 entity: {}                    
-    //             })
-    //             .refs({
-    //               type: {name : className },
-    //               componentTypes: []
-    //             })
-    //             .methods({
-    //               isSameClass: function isSameType(node) {
-    //                 return node.hasOwnProperty('nodeClass') && 
-    //                        node.nodeClass === this.nodeClass
-    //               }
-    //             })
-    //             .init(function initTypes() {
-    //               if(config && config.types) {
-    //                 //for each type put it on the type list for this guy
-    //                 config.types.forEach(function(t) {
-    //                   this.componentTypes.push(t)
-    //                 })
-    //               }
-    //             })
-    //             .static({
-    //               type: this.nodeClass
-    //             })
-
-    return res
-  } 
-
+module.exports = function() {
+  var prototypeType = { name: DEFAULT_TYPE_NAME,
+                      componentTypes: [] }  
+  return stampit()
+            .refs({
+              type: prototypeType,             
+            })
+            .props({
+                previous: undefined,
+                next: undefined,
+                entity: {}                    
+            })
+            .static({
+              type: prototypeType,
+              withName: function withName(name) {     
+                                prototypeType.name = name               
+                                return this
+                            },
+              withComponentTypes: function withNodeTypes(types) {
+                                if( Array.isArray(types) ) {
+                                  types.forEach( function forEachNodeType(t){ 
+                                                    prototypeType.componentTypes.push(t)
+                                                })
+                                } else if(arguments.length > 1) {
+                                  var args = [].slice.call(arguments)
+                                  args.forEach( function forEachArgs(a){
+                                                    prototypeType.componentTypes.push(a)
+                                                })
+                                } else {
+                                  prototypeType.componentTypes.push(types)
+                                }
+                              return this
+                            }            
+            })
+            .methods({
+              hasComponentType: function hasComponentType(t) {
+                return this.type.componentTypes.indexOf(t) > -1
+              }
+            })
+          }
--- src/bz-ash/test/node-test.js
+++ src/bz-ash/test/node-test.js
@@ -1,8 +1,8 @@
 var stampit = require('stampit'),
 	expect = require('chai').expect,
-	NodeFactory = require('../src/bz-ash/nodefactory')
-	EntityPrototype = require('../src/bz-ash/entity')
-	ComponentFactory = require('../src/bz-ash/componentfactory')
+	NodeFactory = require('../nodefactory')
+	EntityPrototype = require('../entity')
+	ComponentFactory = require('../componentfactory')
 
 
 describe('# Node Test',function(){
@@ -15,43 +15,63 @@ describe('# Node Test',function(){
 		expect(node.type.name).to.equal('AnonymousNodeFactory')
 	})
 
-	it('can create a node with family using fluent style',function(){		
-		mockNode = NodeFactory.withName('lymphNode').create()
-		mockNode2 = NodeFactory.withName('soreNode').create()
-		expect(mockNode.type.name).to.equal('lymphNode')
-	})
-
-	it('can create a node with componentTypes using fluent style',function(){		
-
-		mockNode = NodeFactory.withComponentTypes.create()
-		expect(mockNode.type.name).to.equal('lymphNode')
-	})
-
-	it('can create a node with componentTypes',function(){		
-		var PointComponentPrototype = stampit().compose( 
-												ComponentFactory({typeName: 'point'},
-												stampit().props({ x: 0, y: 0, z: 0})))
-		var point = PointComponentPrototype.create()
-
-		 PointNodePrototypeFactory = NodeFactory({typeName: 'point3d', 
-		 										  componentTypes: [point.type]})
-		 pointNode = PointNodePrototypeFactory.create()
-
-		 expect(pointNode).to.exist
-		 expect(PointComponentPrototype.type).to.equal(point.type)
-		 expect(pointNode.componentTypes).to.have.length(1)
-		 expect(pointNode.hasComponentType(point.type)).to.be.true		 
-
-	})
-
 
 	it('default nodes should not have the same type',function() {
 		var mock1 = NodeFactory().create(),
 			mock2 = NodeFactory().create()
 
 			expect(mock1.type).to.not.equal(mock2.type)
+	})	
 
+	it('can create a node with family using fluent style',function(){		
+		mockNode = NodeFactory().withName('lymphNode').create()
+		mockNode2 = NodeFactory().withName('soreNode').create()
+		expect(mockNode.type.name).to.equal('lymphNode')
+	})
 
+	it('can create a node with single component type using fluent style',function(){		
+		var MockComponentPrototype = ComponentFactory(),
+			mockcomponent = MockComponentPrototype.withName('comp1').create(),
+			mockNode = NodeFactory().withComponentTypes(MockComponentPrototype.type).create()
+
+		expect(mockNode.type.componentTypes).to.have.length(1)
+		expect(mockNode.hasComponentType(MockComponentPrototype.type))
+	})
+
+	it('can create a node with multiple component types using fluent style',function(){
+		var CompPrototype1 = ComponentFactory().withName('mockcomp1'),
+			CompPrototype2 = ComponentFactory().withName('mockcomp2'),
+			CompPrototype3 = ComponentFactory().withName('mockcomp3')
+			mocknode = NodeFactory().withComponentTypes(CompPrototype1.type,CompPrototype2.type,CompPrototype3.type).create()
+
+			expect(mocknode.type.componentTypes).to.have.length(3);
+
+			[CompPrototype1.type,
+			 CompPrototype2.type,
+			 CompPrototype3.type].forEach(function(cp) {
+			 								expect(mocknode.hasComponentType(cp)).to.be.true
+										})
+	})
+
+	it('can create a node with an array of component types using fluent style',function(){
+		var CompPrototype1 = ComponentFactory().withName('mockcomp1'),
+			CompPrototype2 = ComponentFactory().withName('mockcomp2'),
+			CompPrototype3 = ComponentFactory().withName('mockcomp3')
+			mocknode = NodeFactory().withComponentTypes([CompPrototype1.type,CompPrototype2.type,CompPrototype3.type]).create()
+
+			expect(mocknode.type.componentTypes).to.have.length(3);
+
+			[CompPrototype1.type,
+			 CompPrototype2.type,
+			 CompPrototype3.type].forEach(function(cp) {
+			 								expect(mocknode.hasComponentType(cp)).to.be.true
+										})
+	})
+
+	it('can create a nodes with different types',function(){		
+		mockNode = NodeFactory().withName('lymphNode').create()
+		mockNode2 = NodeFactory().withName('soreNode').create()
+		expect(mockNode.type.name).to.not.equal(mockNode2.type.name)
 	})
 
 	it('can store and retrieve and entity', function() {
